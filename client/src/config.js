5377045dad6c3a735123b2de8605f467c20e5f23
Hans Trautlein <hans@trautlein.com>
2016-11-01 17:10:51 -0700

    Fixed error with config.js file

--- client/src/config.js
+++ client/src/config.js
@@ -3,7 +3,7 @@ const addedHostname = '';
 const prepend = 'http://';
 const localhost = 'http://localhost';
 const serverPort = '3000';
-const url = prepend + ( addedHostname ? addedHostname : hostname ) + ":" + serverPort;
+const url = prepend + ( addedHostname ? addedHostname : localhost ) + ":" + serverPort;
 
 process.env.BASE_URL = url;
 
