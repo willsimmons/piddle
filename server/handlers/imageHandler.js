68e0905780e1f317e8cb89e2c83217c27df3abee
Meredith Nachman <mwnachman@gmail.com>
2016-11-04 11:05:18 -0700

    Refactor total and subtotal

--- server/handlers/imageHandler.js
+++ server/handlers/imageHandler.js
@@ -43,16 +43,37 @@ var isFloat = function(n){
   return ((Number(n) == n) && (n.includes(".")) && (Number(n) * 100 % 1 === 0));
 };
 
-var isUsefulMoneyWord = function(str) {
+var isTaxWord = function(str) {
   if (str.includes("Tax") ||
       str.includes("TAX") ||
-      str.includes("tax") ||
-      str.includes("Due") ||
-      str.includes("DUE") ||
-      str.includes("due") ||
-      str.includes("Total") ||
+      str.includes("tax")
+     ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var isTotalWord = function(str) {
+  if (str.includes("Total") ||
       str.includes("TOTAL") ||
       str.includes("total") ||
+      str.includes("Subtotal") ||
+      str.includes("SUBTOTAL") ||
+      str.includes("subtotal") ||
+      str.includes("Due") ||
+      str.includes("DUE") ||
+      str.includes("due") 
+     ) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+var isUsefulMoneyWord = function(str) {
+  if (isTotalWord(str) ||
+      isTaxWord(str) ||
       str.includes("$")
      ) {
     return true;
@@ -63,19 +84,17 @@ var isUsefulMoneyWord = function(str) {
 };
 
 var isUnhelpfulMoneyWord = function(str) {
-  if (str.includes("Price") ||
-      str.includes("Total") ||
-      str.includes("TOTAL") ||
-      str.includes("total") ||
+  if (isTotalWord(str) ||
+      str.includes("Price") ||
       str.includes("Reprint") ||
       str.includes("Server") ||
       str.includes("SERVER") ||
-      str.includes(":") ||
-      str.includes("/") ||
       str.includes("GUEST") ||
       str.includes("Guest") ||
       str.includes("GUESTS") ||
-      str.includes("Guests")
+      str.includes("Guests") ||
+      str.includes(":") ||
+      str.includes("/") 
      ) {
     return true;
   } else {
@@ -87,43 +106,50 @@ var toTuples = function(arr) {
   var results = {};
   var tuples = [];
   var tax;
+
   for (var i = 0; i < arr.length; i+=2) {
     var tuple = {};
     if (isFloat(arr[i])) {
+      tuple.quantity = 1;
       tuple.price = arr[i];
       tuple.description = arr[i+1];
-      tuple.quantity = 1;
     } else {
       if (arr[i+1] === undefined) {
         arr[i+1] = 0;
       }
+      tuple.quantity = 1;
       tuple.price = arr[i+1];
       tuple.description = arr[i];
-      tuple.quantity = 1;
     }
     tuples.push(tuple);
-  }
+  };
+
   for (var i = 0; i < tuples.length; i++) {
     var str1 = String(tuples[i]['price']);
     var str2 = String(tuples[i]['description']);
-    if (str1.includes("tax") || str1.includes("TAX") || str1.includes("Tax")) {
+    if (isTaxWord(str1)) {
       var str3 = tuples[i]['price'];
       tuples[i]['price'] = str3.match(/[0-9.]+\d*/g);
       tax = Number(tuples[i]['price']);
       tuples.splice(i, 1);
+      i--;
     }
-    if (str2.includes("tax") || str2.includes("TAX") || str2.includes("Tax")) {
+    if (isTaxWord(str2)) {
       tax = Number(tuples[i]['price']);
       tuples.splice(i, 1);
+      i--;
     }
-  }
+  };
+
   for (var i = 0; i < tuples.length; i++) {
     var str1 = String(tuples[i]['price']);
     var str2 = String(tuples[i]['description']);
-    if (!str1.match(/[0-9]\d*/g) && !str2.match(/[0-9]\d*/g)) {
+    if (isTotalWord(str1) || isTotalWord(str2)) {
       tuples.splice(i, 1);
+      i--;
     }
-  }
+  };
+
   for (var i = 0; i < tuples.length; i++) {
     tuples[i]['price'] = Number(tuples[i]['price']) || 0;
     var str = String(tuples[i]['description']);
@@ -136,7 +162,7 @@ var toTuples = function(arr) {
       tuples[i]['quantity'] = Number(str[0]);
       tuples[i]['description'] = tuples[i]['description'].slice(2);
     }
-  }
+  };
 
   results.tuples = tuples;
   results.tax = tax;
