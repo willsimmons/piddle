465f7f02a31abf99ccfd7f0545f50b3a8f545f0b
Ubuntu <ubuntu@ip-172-31-33-56.us-west-2.compute.internal>
2016-11-05 20:55:52 +0000

    Add HTTPS support, thanks to @therealgilles

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config({silent: true});
+const fs = require('fs');
+const https = require('https');
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -37,7 +39,22 @@ app.use('*', (request, response) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port);
 
-console.log(`Server listening on port: ${port}`);
+const sslPath = process.env.SSL_PATH || '/etc/letsencrypt/live/tendr.trautlein.com';
+const options = {
+  key: fs.readFileSync(sslPath + '/privkey.pem'),
+  cert: fs.readFileSync(sslPath + '/fullchain.pem')
+};
+
+// secure server setup
+const server = https.createServer(options, app);
+server.listen(port, err => {
+  if (err) {
+    error('Error while trying to start the server (port already in use maybe?)');
+    return err;
+  }
+  console.log(`secure server listening on port ${port}`);
+});
+
+
 module.exports = app;
